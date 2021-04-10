7521db228f045ecca3b12f79cafe05cf45f91e23
cmcmone <cmcmone@gmail.com>
2021-04-10 18:01:53 -0400

    update css config

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,5 @@
 // postcss.config.js
 module.exports = {
-  purge: [
-    // Use *.tsx if using TypeScript
-    './pages/**/*.js',
-    './components/**/*.js'
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {},
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
