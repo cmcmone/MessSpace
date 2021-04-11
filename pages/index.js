006f13434fbda82b64b45be89bfc6357b3547f7e
cmcmone <cmcmone@gmail.com>
2021-04-11 01:01:00 -0400

    Add react-icons to use icon font

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { FaBeer } from 'react-icons/fa';
+
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-center text-lg text-gray-600">Hello World!</h1>
+      <h1 className="inline-block text-center bg-red-300 text-lg text-gray-600">
+        <FaBeer className="inline-block text-green-700 mb-1" />
+        Hello World!
+      </h1>
     </div>
   );
 }
