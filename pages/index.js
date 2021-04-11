fea9ed1a5d5ffc87bd65e18bd4e0f8ce50908aa5
cmcmone <cmcmone@gmail.com>
2021-04-10 23:24:51 -0400

    Init index page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import ChitChat from '../components/chitchat'
-
 export default function Home() {
   return (
-    <ChitChat />
+    <div className="container mx-auto">
+      <h1 className="text-center text-lg text-gray-600">Hello World!</h1>
+    </div>
   );
 }
