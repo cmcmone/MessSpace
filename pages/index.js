a5747c24716e5f54e03c5e8d11902414c30a23af
cmcmone <cmcmone@gmail.com>
2021-04-11 12:46:44 -0400

    Use Font Awesome instead of React-icons

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import { FaBeer } from 'react-icons/fa';
-
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <h1 className="inline-block text-center bg-red-300 text-lg text-gray-600">
-        <FaBeer className="inline-block text-green-700 mb-1" />
-        Hello World!
-      </h1>
-    </div>
+    <>
+      <h1>Hello World!</h1>
+    </>
   );
 }
