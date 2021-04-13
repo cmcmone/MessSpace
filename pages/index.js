1f71c44c99f7de58ea04c36e5f5a5df6aa2abdf9
cmcmone <cmcmone@gmail.com>
2021-04-13 12:00:15 -0400

    Updata index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
+import Head from 'next/head';
+
+import About from '../components/about';
+import Contact from '../components/contact';
+import Education from '../components/education';
+import Hero from '../components/hero';
+import Navbar from '../components/navbar';
+import Projects from '../components/project';
+import Footer from '../components/Footer'
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Head>
+        <title>Mess Space</title>
+      </Head>
+      <header className="font-lato text-white bg-gray-600">
+        <Navbar />
+        <Hero />
+      </header>
+      <article className="container max-w-4xl font-lato">
+        <About />
+        <Projects />
+        <Education />
+        <Contact />
+      </article>
+      <footer className="bg-gray-200">
+        <Footer />
+      </footer>
     </>
   );
 }
